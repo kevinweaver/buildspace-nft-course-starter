63796d819fc50f6036432ec03fca23f47cd290ab
Kevin Weaver <kevinweaver2@gmail.com>
2021-10-21 20:45:29 -0400

    Add connect button logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -9,6 +9,58 @@ const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
+
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account)
+    } else {
+        console.log("No authorized account found")
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      /*
+      * Fancy method to request access to account.
+      */
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      /*
+      * Boom! This should print out public address once we authorize Metamask.
+      */
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
   // Render Methods
   const renderNotConnectedContainer = () => (
     <button className="cta-button connect-wallet-button">
@@ -20,11 +72,19 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">My NFT Collection</p>
+          <p className="header gradient-text">Unbox Your Truffle</p>
           <p className="sub-text">
-            Each unique. Each beautiful. Discover your NFT today.
+            Unique, delicious, chocolate NFTs.
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount === "" ? (
+            <button onClick={connectWallet} className="cta-button connect-wallet-button">
+              Connect to Wallet
+            </button>
+          ) : (
+            <button onClick={null} className="cta-button connect-wallet-button">
+              Mint Truffle
+            </button>
+          )}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
@@ -33,7 +93,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built with @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
